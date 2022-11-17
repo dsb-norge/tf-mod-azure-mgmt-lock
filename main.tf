88310276beeb5e313932e69e63e68d940641e45d
Even Lislebø <even.lislebo@dsb.no>
2022-11-17 10:38:21 +0100

    tolerate null input value for lock level

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ resource "azurerm_management_lock" "protected_resource_lock" {
   for_each   = var.protected_resources
   name       = "lock-${each.value.name}"
   scope      = each.value.id
-  lock_level = each.value.lock_level != null ? each.value.lock_level : "CanNotDelete"
+  lock_level = coalesce(each.value.lock_level, "CanNotDelete")
   notes      = <<-EOF
     ApplicationName: ${var.app_name}
     CreatedBy: ${var.created_by}
-    Description: ${each.value.lock_level} lock for ${each.value.name}
+    Description: ${coalesce(each.value.lock_level, "CanNotDelete")} lock for ${each.value.name}
   EOF
 }
